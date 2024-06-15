2448cb67d06ca68adddd21bc193b2e595d476586
sbx_user1051 <sbx_user1051@169.254.122.181>
2024-06-15 19:19:23 +0000

    Sat Jun 15 19:19:23 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,6 +6,8 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         int number_A = 0;
         int number_B = 0;
+        int all_B = 0;
+        int number_E = 0;
         int price = 0;
         for (int c=0; c < skus.length(); c++) {
             if (skus.charAt(c) == 'A') {
@@ -14,10 +16,15 @@ public class CheckoutSolution {
                 if (number_A == 3) {
                     price -= 150;
                     price += 130;
+                }
+                if (number_A == 5) {
+                    price -= 230;
+                    price += 200;
                     number_A = 0;
                 }
             }
             else if (skus.charAt(c) == 'B') {
+                all_B ++;
                 number_B++;
                 price += 30;
                 if (number_B == 2) {
@@ -32,6 +39,16 @@ public class CheckoutSolution {
             else if (skus.charAt(c) == 'D') {
                 price += 15;
             }
+            else if (skus.charAt(c) == 'E') {
+                number_E ++;
+                price += 40;
+                if (number_E == 2) {
+                    if (all_B > 0) {
+                        all_B--;
+                        price -= 30;
+                    }
+                }
+            }
             else {
                 return -1;
             }
@@ -39,6 +56,7 @@ public class CheckoutSolution {
         return price;
     }
 }
+
 
 
 
